31b23d736666080b1e2f0d836494f6f6cb49d1fb
juniorAkp <juniorAkwei2@gmail.com>
2025-01-16 23:45:54 +0000

    Update generateTokenAndSetCookie.js

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -5,8 +5,9 @@ const jwt = require('jsonwebtoken')
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000
+    sameSite: "None",
+    maxAge: 24 * 60 * 60 * 1000,
+    path:"/"
   })
   return token
 }
