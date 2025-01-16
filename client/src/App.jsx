faa4ee5744e05b2a1ef3f8a49c9073b9bbe24bbd
juniorAkp <juniorAkwei2@gmail.com>
2025-01-16 22:48:50 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import UserEditDetails from './pages/Edit.jsx';
 import OrderPage from './pages/OrderPage.jsx';
 import FeaturedProducts from './pages/FeaturedProducts.jsx';
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = 'https://project-ecom-x7fx.onrender.com'
 axios.defaults.withCredentials = true;
 
 
