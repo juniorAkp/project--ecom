88cff9d6df118989bb5ba346a580304d109e3b60
juniorAkp <juniorAkwei2@gmail.com>
2025-01-17 19:59:44 +0000

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -61,11 +61,10 @@ const login = async(req,res)=>{
 const logout = async (req, res) => {
     try {
         res.clearCookie('token',{
-          httpOnly: true,
-          domain:".onrender.com",
-          secure: process.env.NODE_ENV != "development",
-          sameSite: "None",
-          maxAge: 24 * 60 * 60 * 1000,
+           httpOnly: true,
+           secure: process.env.NODE_ENV !== "development",
+           sameSite: "None",
+           maxAge: 24 * 60 * 60 * 1000,
         });
       
         res.status(200).json({ message: 'Logged out successfully' });
