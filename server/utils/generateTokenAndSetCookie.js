42cb7afab8fe05f092d0ab2a9c16f183acbb820b
juniorAkp <juniorAkwei2@gmail.com>
2025-01-17 16:38:53 +0000

    Update generateTokenAndSetCookie.js

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -15,7 +15,6 @@ const generateTokenAndSetCookie2 = async(res,userId,isAdmin)=>{
   res.cookie("token2", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: default,
     maxAge: 24 * 60 * 60 * 1000,
   })
   return token
