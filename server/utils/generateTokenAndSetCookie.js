1949dbc74df0f33c81024dd77c0f25b418380670
juniorAkp <juniorAkwei2@gmail.com>
2025-01-17 15:31:56 +0000

    Update generateTokenAndSetCookie.js

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken')
 }
 const generateTokenAndSetCookie2 = async(res,userId,isAdmin)=>{
   const token = jwt.sign({userId,isAdmin}, process.env.JWT_SECRET, {expiresIn: '1d'})
-  res.cookie("token", token, {
+  res.cookie("token2", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "None",
