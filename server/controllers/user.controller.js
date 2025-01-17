a8426357c135f61dafdd40e53ce8effd1dbe1c12
juniorAkp <juniorAkwei2@gmail.com>
2025-01-17 14:31:24 +0000

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -62,7 +62,7 @@ const logout = async (req, res) => {
         res.clearCookie('token',{
           httpOnly: true,
           secure: process.env.NODE_ENV !== "development",
-          sameSite: "None",
+          sameSite: "Lax",
           maxAge: 24 * 60 * 60 * 1000,
         }
           );
