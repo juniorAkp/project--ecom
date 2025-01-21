e85d6ba3ca5152fde1b11083cdabdee9927d7d77
nii <juniorAkwei2@gmail.com>
2025-01-21 19:46:01 +0000

    update category name

--- client/src/pages/admin/displayAllproducts.jsx
+++ client/src/pages/admin/displayAllproducts.jsx
@@ -70,7 +70,7 @@ const DisplayAllProducts = () => {
                     <tr key={product.id} className="border-b">
                       <td className="px-4 py-2">{product.name}</td>
                       <td className="px-4 py-2">${product.price}</td>
-                      <td className="px-4 py-2">{product.category.name}</td>
+                      <td className="px-4 py-2">{product.category}</td>
                       <td className="px-4 py-2">{product.countInStock}</td>
                       <td className="px-4 py-2 flex flex-wrap space-x-2 justify-start">
                         <Link
